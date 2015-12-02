9a6726f4b39222a303579fcd4adc2651705894b8
Andrzej Nagórko <amn@mimuw.edu.pl>
2015-12-02 18:24:06 +0100

    update

--- 5dpaper/table.py
+++ 5dpaper/table.py
@@ -14,19 +14,45 @@ db = MySQLdb.connect(host="127.0.0.1", # your host, usually localhost
 
 cur = db.cursor() 
 
+def symmetryClass(b):
+    if b[0] == b[1] and b[1] == b[2] and b[2] == b[3]:
+        return b
+
+    while b[0] < b[1] or b[0] < b[2] or b[0] < b[3]:
+        b = b[1:] + b[:1]
+
+    if (b[1] < b[3]):
+        b[1], b[3] = b[3], b[1]
+
+    if (b[1] < b[0] and b[2] < b[0] and b[3] < b[0]):
+        return b
+
+    if b[0] == b[3] or b[0] == b[1]:
+        while not b[1] == b[0]:
+            b = b[1:] + b[:1]
+        if (b[2] < b[3]):
+            b[2], b[3] = b[3], b[2]
+    return b
+
 def query():
    cur.execute("select * from tree order by r_bound desc")
 
+def box(r):
+    s = map(int, r.split(' '))
+    s = [ s[0], s[2], s[4], s[6] ]
+    return symmetryClass(s)
+    
 def bbox(r):
-    s = map(int,r.split(' '))
-    return "(" + str((s[0] - 18) / 4) + ", " + str((s[2]-18)/4) + ", " + str((s[4]-18)/4)\
-            + ", " + str((s[6]-18)/4) + ")"
+    s = box(r)
+    return "(" + str((s[0] - 18) / 4) + ", " + str((s[1]-18)/4) + ", " + str((s[2]-18)/4)\
+            + ", " + str((s[3]-18)/4) + ")"
 
+    
 def start_table():
     print """%
     \\begin{tabular}{|l|l|l|l|}
     \\hline
-    No &  Bounding box &  Max size  \\\\
+    No & BBox & Size  \\\\
     \\hline%
     """
 
@@ -38,19 +64,25 @@ def end_table():
 query()
 rows = cur.fetchall()
 
-
 no = 1
 start_table()
+
+done = []
 
 for row in rows:
     if row[7] == None:
         continue
     if row[7] < 83:
         continue
+
+    if box(row[4]) in done:
+        continue
     
+    done.append(box(row[4]))
+
     print str(no) + "&" + bbox(row[4]) + "& " + str(row[7]) + "\\\\"
     no = no + 1
-    if no == 17:
+    if no == 11 or no == 21:
         end_table()
         print "\\hspace*{5mm}"
         start_table()
